9a4d18fc0e41757d1b3116112d6156d405ce4fe5
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-14 11:09:09 +0700

    membuat fungsi main

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -18,3 +18,32 @@ public:
         setID();
     }
 };
+
+int mahasiswa::nim = 167;
+
+void mahasiswa::setID() {
+    id = ++nim;
+}
+
+void mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("Lia Kurnia");
+    mahasiswa mhs2("Asroni");
+    mahasiswa mhs3("Andi Kurniawan");
+    mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    system("pause");
+
+    return 0;
+}
