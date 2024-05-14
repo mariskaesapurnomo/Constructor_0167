4560e5aec2aeb02dc18f862248704564f2db6d75
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-14 13:20:35 +0700

    objek constructor

--- construktorproject/construktorproject.cpp
+++ construktorproject/construktorproject.cpp
@@ -34,4 +34,20 @@ void mahasiswa::cetak() {
     cout << endl << "Nim =" << nim << endl;
     cout << " Nama " << nama << endl;
 }
+
+int main()
+{
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("friska");
+    mahasiswa mhs4(30, "fina");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+    system("pause");
+    return 0;
+}
+
     
