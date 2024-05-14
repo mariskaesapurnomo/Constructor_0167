6cc1d650a0fd9db3cd1b24132e5b08f417f2290d
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-14 14:17:15 +0700

    chain function calls

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -18,3 +18,11 @@ public:
 		return this->judul;
 	}
 } bukunya;
+
+int main()
+{
+	//bukunya.setJudul("Matematika");
+	//cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul();	//chain function calls
+	return 0;
+}
